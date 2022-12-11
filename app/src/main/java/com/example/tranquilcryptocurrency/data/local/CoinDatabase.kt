e3d4f0011cf428eab777d26d7e96a98db13108be
Rudione <denisrudik01@gmail.com>
2022-12-11 19:04:56 +0200

    features: add database

--- app/src/main/java/com/example/tranquilcryptocurrency/data/local/CoinDatabase.kt
+++ app/src/main/java/com/example/tranquilcryptocurrency/data/local/CoinDatabase.kt
@@ -4,28 +4,27 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.example.tranquilcryptocurrency.data.remote.dto.CoinDto
 
 @Database(entities = [CoinDto::class], version = 1)
-abstract class MealDatabase: RoomDatabase() {
+abstract class CoinDatabase: RoomDatabase() {
     abstract fun coinDao(): CoinDao
 
     companion object {
         @Volatile
-        var INSTANCE: MealDatabase? = null
+        var INSTANCE: CoinDatabase? = null
 
         @Synchronized
-        fun getInstance(context: Context): MealDatabase {
+        fun getInstance(context: Context): CoinDatabase {
             if(INSTANCE == null) {
                 INSTANCE = Room.databaseBuilder(
                     context,
-                    MealDatabase::class.java,
-                    "meal.db"
+                    CoinDatabase::class.java,
+                    "coin.db"
                 ).fallbackToDestructiveMigration()
                     .build()
             }
-            return INSTANCE as MealDatabase
+            return INSTANCE as CoinDatabase
         }
     }
 }
